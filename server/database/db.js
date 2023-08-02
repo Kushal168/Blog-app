d3e206804fb7daef543bd9a2bb3235d2a6b8130b
Kushal <Kushalgoyal168@gmail.com>
2023-08-02 18:58:19 +0530

    change in URL

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    const URL = process.env.MONOGO_URL;
+    const URL = `mongodb://localhost:27017/blog`;
+    // const URL = process.env.MONOGO_URL;
     // const URL =`mongodb://kushalgoyal168:4tCYbxWgYOv6O50n@ac-vrfhmri-shard-00-00.g1t5dwe.mongodb.net:27017,ac-vrfhmri-shard-00-01.g1t5dwe.mongodb.net:27017,ac-vrfhmri-shard-00-02.g1t5dwe.mongodb.net:27017/?ssl=true&replicaSet=atlas-fjui11-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
         await mongoose.connect(URL, { useNewUrlParser: true })
