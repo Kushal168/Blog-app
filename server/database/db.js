d26f05abe9194fa01511b91ea77db6af166087bc
Kushal <Kushalgoyal168@gmail.com>
2023-08-02 19:03:54 +0530

    version2.2or later

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
 const Connection = async (username, password) => {
-    const URL = `mongodb://localhost:27017/blog`;
+
+    // const URL = `mongodb://localhost:27017/blog`;
+    const URL = `mongodb://kushalgoyal168:4tCYbxWgYOv6O50n@ac-vrfhmri-shard-00-00.g1t5dwe.mongodb.net:27017,ac-vrfhmri-shard-00-01.g1t5dwe.mongodb.net:27017,ac-vrfhmri-shard-00-02.g1t5dwe.mongodb.net:27017/?ssl=true&replicaSet=atlas-fjui11-shard-0&authSource=admin&retryWrites=true&w=majority`;
     // const URL = process.env.MONOGO_URL;
     // const URL =`mongodb://kushalgoyal168:4tCYbxWgYOv6O50n@ac-vrfhmri-shard-00-00.g1t5dwe.mongodb.net:27017,ac-vrfhmri-shard-00-01.g1t5dwe.mongodb.net:27017,ac-vrfhmri-shard-00-02.g1t5dwe.mongodb.net:27017/?ssl=true&replicaSet=atlas-fjui11-shard-0&authSource=admin&retryWrites=true&w=majority`;
     try {
